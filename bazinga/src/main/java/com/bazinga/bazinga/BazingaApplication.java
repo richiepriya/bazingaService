41c730c970370b360541259b500dc1fde4dfad72
richiepriya <41781231+richiepriya@users.noreply.github.com>
2020-07-09 00:56:35 +0530

    Add files via upload
    initial commit

---
+++ bazinga/src/main/java/com/bazinga/bazinga/BazingaApplication.java
@@ -0,0 +1,13 @@
+package com.bazinga.bazinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazingaApplication.class, args);
+	}
+
+}
---
+++ bazinga/src/test/java/com/bazinga/bazinga/BazingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazinga.bazinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazingaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
